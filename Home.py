88e0b0627681348da87ecc00b127e24f06ddabb7
Manikandan P <manikandanp@softsquare.biz>
2024-08-09 15:55:13 +0530

    changes

--- Home.py
+++ Home.py
@@ -9,6 +9,12 @@ from dateutil.relativedelta import relativedelta
 
 st.set_page_config(layout="wide")
 
+# data_url = "https://i.pinimg.com/originals/5d/35/e3/5d35e39988e3a183bdc3a9d2570d20a9.gif"
+# st.image(
+#             data_url, 
+#             width=400,
+#         )
+
 st.markdown("""
     <h1 id="chat-header" style="position: fixed;
                    top: 0;
@@ -18,7 +24,7 @@ st.markdown("""
                    background-color: #f1f1f1;
                    color: #008080;
                    z-index: 9">
-        Softsquare's Products : Dev App Analytics
+        Softsquare's Products: Dev App Analytics
     </h1>
 """, unsafe_allow_html=True)
 
@@ -44,137 +50,158 @@ if 'filtes' not in st.session_state:
         }
     ]
 
-col1, col2, col3, col4 = st.columns(4)
 
-# print('session Name :::::::', st.session_state.filtes[0]['isFilter'])
+col1, col2, col3, col4, col5 = st.columns(5)
+
+next_month =date.today().replace(day=28) + timedelta(days=4) - relativedelta(months = 1)
+toDateStandard = next_month - timedelta(days=next_month.day)  
 
 with col1:
     productNameSelect = st.selectbox( "Select Product", ("AGrid", "User 360", "Media Manager", "Snap Data"),  index=None) 
-    next_month =date.today().replace(day=28) + timedelta(days=4) - relativedelta(months = 1)
-    toDateStandard = next_month - timedelta(days=next_month.day)
+    #  "Lightning Image Slider", "Lightning DataTable Dev", "Lead Assignment", "Response Time Tracker", "Opportunity Stage Alert" 
 
-with col3:
+with col2:
     fromDate = st.date_input("From", toDateStandard - relativedelta(years = 1))
     if(fromDate != None):
         st.session_state.filtes[0]['isFilter'] = True
         st.session_state.filtes[0]['isDateFromFilter'] = True
         st.session_state.filtes[0]['fromDate'] = str(fromDate)
     
-        # st.write("from:", fromDate)
-with col4:
+with col3:
     toDate = st.date_input("To", toDateStandard)
     if(toDate != None):
         st.session_state.filtes[0]['isFilter'] = True
         st.session_state.filtes[0]['isDateToFilter'] = True
         st.session_state.filtes[0]['toDate'] = str(toDate)
 
-        # st.write("to:", toDate)
+with col4:
 
-if(productNameSelect != None and fromDate != None  and toDate != None):
-    print('start.....')
-    dbConnect = DataBase.connectSnowflake()
-    orgNames = DataBase.getOrganizationName(dbConnect, productNameSelect)
+    if(productNameSelect != None and fromDate != None  and toDate != None):
+        dbConnect = DataBase.connectSnowflake()
+        orgNames = DataBase.getOrganizationName(dbConnect, productNameSelect)
+        if(orgNames and len(orgNames) > 0):
+            orgNames[0]['combined'] = orgNames[0]['ORGANIZATION_NAME'] + '-' + orgNames[0]['ORGANIZATION_ID']
+            
+            filterOrgName = st.selectbox("Select Organization", (orgNames[0]['combined'] ), index=None)
+            if(filterOrgName != None):
+                st.session_state.filtes[0]['isFilter'] = True
+                st.session_state.filtes[0]['isOrgFilter'] = True
+                st.session_state.filtes[0]['orgName'] = filterOrgName
+            else:
+                st.session_state.filtes[0]['isOrgFilter'] = False
+    else:
+        filterOrgName = st.selectbox("Select Organization", ([''] ), index=None, disabled=True)
+
+with col5:
 
     if(productNameSelect == 'AGrid'):
         featureList = userData.getAgridFeatureList()
     elif(productNameSelect == 'Media Manager'):
         featureList = userData.getMediaManagerFeatureList()
+        
+    if productNameSelect != None and len(featureList) > 0:
+        featureFilter = st.selectbox("Select Feature",(featureList[0].keys()), index=None)
+        if(featureFilter != None):
+            st.session_state.filtes[0]['isFilter'] = True
+            st.session_state.filtes[0]['isFeatureFilter'] = True
+            st.session_state.filtes[0]['featuesName'] = featureList[0].get(featureFilter)
+        else:
+            st.session_state.filtes[0]['isFeatureFilter'] = False        
+
+# with st.spinner('Wait for it...'):
+
+# userCountCol, CreateCountCol = st.columns(2)
+
+# with userCountCol:
+#     dbConnect = DataBase.connectSnowflake()
+#     userCount = DataBase.getnumberOfUsers(dbConnect, productNameSelect, st.session_state.filtes[0])
+
+  
+if(productNameSelect != None and fromDate != None  and toDate != None):
+    print('start.....')
 
     with st.container(border=True):
-        col1, col2, col3 = st.columns(3)
 
-        # with col2:
-        #     featureFilter = st.selectbox("Select Feature", ( featureList[0].keys()), index=None)
-        #     if(featureFilter != None):
-        #         st.session_state.filtes[0]['isFilter'] = True
-        #         st.session_state.filtes[0]['isFeatureFilter'] = True
-        #         st.session_state.filtes[0]['featuesName'] = featureList[0].get(featureFilter)
-        #     else:
-                # st.session_state.filtes[0]['isFeatureFilter'] = False
+        col_inside1, col_inside2, col_inside3 = st.columns(3)
 
-        with col3:
-            if(orgNames and len(orgNames) > 0):
-                orgNames[0]['combined'] = orgNames[0]['ORGANIZATION_NAME'] + '-' + orgNames[0]['ORGANIZATION_ID']
-                # print('orgNames :::::::::::::::::::', orgNames)
+        getProductDetails = DataBase.getProductDetails(dbConnect, productNameSelect, st.session_state.filtes[0])
+
+        featureListValues = featureList[0].values()
+        featureListValuesList = tuple(featureListValues)
+
+        getProductDetailsFeatures = DataBase.getProductDetailsWithfeature(dbConnect, productNameSelect, st.session_state.filtes[0], featureListValuesList)
+
+        getProductDetailsFeaturesDf = getProductDetailsFeatures[0]
+
+        def getNameFromFeatuesList(row):
+                cutomEntity = row['CUSTOM_ENTITY']
+                if not pd.isna(cutomEntity) or str(cutomEntity).lower() != 'nan':
+                    data = userData.getAgridFeatureList()
+                    return next((key for dictionary in data for key, value in dictionary.items() if value.lower() == cutomEntity.lower()), None)
+                else:
+                    return cutomEntity
                 
-                filterOrgName = st.selectbox("Select Organization", (orgNames[0]['combined'] ), index=None)
-                if(filterOrgName != None):
-                    st.session_state.filtes[0]['isFilter'] = True
-                    st.session_state.filtes[0]['isOrgFilter'] = True
-                    st.session_state.filtes[0]['orgName'] = filterOrgName
-                else:
-                    st.session_state.filtes[0]['isOrgFilter'] = False
-
-
-        print('filterOrgName ::::::::::::::::', filterOrgName)
-        # if(filterOrgName != None and len(filterOrgName) > 0 ):
-        # getProductDetails = DataBase.getProductDetails(dbConnect, productNameSelect, True, filterOrgName)
+        getProductDetailsFeaturesDf['NEW_CUSTOM_ENTITY'] = getProductDetailsFeaturesDf.apply(getNameFromFeatuesList, axis=1)
         
-        # else:
-        #     getProductDetails = DataBase.getProductDetails(dbConnect, productNameSelect, False, None)
+        if(getProductDetails != None and len(getProductDetails) > 0 ):
 
-        
-        with st.container(border=True):
-            col_inside1, col_inside2, col_inside3 = st.columns(3)
-
-            with col_inside1:
-                title = 'Over all Usage of ' + productNameSelect
-                st.write(title)
-
-                featureFilter = st.selectbox("Select Feature", ( featureList[0].keys()), index=None, key='featureFilter')
-
-                if(featureFilter != None):
-                    st.session_state.filtes[0]['isFilter'] = True
-                    st.session_state.filtes[0]['isFeatureFilter'] = True
-                    st.session_state.filtes[0]['featuesName'] = featureList[0].get(featureFilter)
-                else:
-                    st.session_state.filtes[0]['isFeatureFilter'] = False
-
-            #### Over All Usage
-            getProductDetails = DataBase.getProductDetails(dbConnect, productNameSelect, st.session_state.filtes[0])
-
-            if(getProductDetails != None and len(getProductDetails) > 0 ):
-                # title = 'Over all Usage of ' + productNameSelect
-                fig = px.bar(getProductDetails[0], x = "LOG_CREATED_MONTH", y = "OVER_ALL_USAGE", labels={"OVER_ALL_USAGE" : "Total Usage", 'LOG_CREATED_MONTH' : 'Months'},
-                             hover_data=["OVER_ALL_USAGE"],
-                            template="gridon",height=500)
-                st.plotly_chart(fig,use_container_width=True)
-
-        #### Top 10 and least 10 features
-
-        if(len(featureList) > 0):
-            featureListValues = featureList[0].values()
-            featureListValuesList = tuple(featureListValues)
-
-            if(filterOrgName != None and len(filterOrgName) > 0):
-                getFeaturesDetails = DataBase.getFeatureDetails(dbConnect, productNameSelect, featureListValuesList, True, filterOrgName)
-            else:
-                getFeaturesDetails = DataBase.getFeatureDetails(dbConnect, productNameSelect, featureListValuesList, False, None)
-
+            # Dashboard 1
+            title = 'Monthly Utilization Of ' + productNameSelect
+            fig = px.bar(getProductDetails[0], x = "LOG_CREATED_MONTH", y = "OVER_ALL_USAGE", labels={"OVER_ALL_USAGE" : "Total Usage", 'LOG_CREATED_MONTH' : 'Months'},
+                        hover_data=["OVER_ALL_USAGE"],
+                        template="gridon",height=500, title = title)
+            st.plotly_chart(fig,use_container_width=True)
             
-            if(len(getFeaturesDetails) > 0):
-                def getNameFromFeatuesList(row):
-                    cutomEntity = row['CUSTOM_ENTITY']
-                    if not pd.isna(cutomEntity) or str(cutomEntity).lower() != 'nan':
-                        data = userData.getAgridFeatureList()
-                        return next((key for dictionary in data for key, value in dictionary.items() if value.lower() == cutomEntity.lower()), None)
-                    else:
-                        return cutomEntity
-                
-                df = getFeaturesDetails[0]
-                df['NEW_CUSTOM_ENTITY'] = df.apply(getNameFromFeatuesList, axis=1)
-                title = 'Most Used ' + productNameSelect + ' Features'
-                fig = px.bar(df, x = "NEW_CUSTOM_ENTITY", y = "TOTAL", labels={"TOTAL" : "Total Usage", 'NEW_CUSTOM_ENTITY' : 'Features'},
-                            title = title, hover_data=["TOTAL"],
-                            template="gridon",height=500)
-                st.plotly_chart(fig,use_container_width=True)
+            # Dashboard 2
+            if(featureFilter == None):
+                figfeatures = px.bar(getProductDetailsFeaturesDf, x="LOG_CREATED_MONTH", y="OVER_ALL_USAGE", color="NEW_CUSTOM_ENTITY", title="Features Utilization")
+                st.plotly_chart(figfeatures,use_container_width=True)
 
-        packageVersion = DataBase.getPackageVersion(dbConnect, productNameSelect, st.session_state.filtes[0])
-        title = 'Package Version Usage of ' + productNameSelect
-        fig = px.bar(packageVersion[0], x = "VERSION_NAME", y = "TOTAL", labels={"TOTAL" : "Total Usage", "VERSION_NAME" : 'Package Version Name'},
-                    title = title, hover_data=["TOTAL"],
-                    template="gridon",height=500)
-        st.plotly_chart(fig,use_container_width=True)
+
+            # Dashboard 3
+            if(featureFilter != None  and filterOrgName == None):
+                featuresUsageWithOrg = DataBase.getFeaturesUsageWithOrg(dbConnect, productNameSelect, st.session_state.filtes[0])
+                figfeatures = px.bar(featuresUsageWithOrg[0], x="LOG_CREATED_MONTH", y="OVER_ALL_USAGE", color="ORGANIZATION_NAME", title="Feature Usage by Organization")
+                st.plotly_chart(figfeatures,use_container_width=True)
+                
+
+
+
+
+
+    # if(len(featureList) > 0):
+    #     featureListValues = featureList[0].values()
+    #     featureListValuesList = tuple(featureListValues)
+
+    #     if(filterOrgName != None and len(filterOrgName) > 0):
+    #         getFeaturesDetails = DataBase.getFeatureDetails(dbConnect, productNameSelect, featureListValuesList, True, filterOrgName)
+    #     else:
+    #         getFeaturesDetails = DataBase.getFeatureDetails(dbConnect, productNameSelect, featureListValuesList, False, None)
+
+        
+    #     if(len(getFeaturesDetails) > 0):
+    #         def getNameFromFeatuesList(row):
+    #             cutomEntity = row['CUSTOM_ENTITY']
+    #             if not pd.isna(cutomEntity) or str(cutomEntity).lower() != 'nan':
+    #                 data = userData.getAgridFeatureList()
+    #                 return next((key for dictionary in data for key, value in dictionary.items() if value.lower() == cutomEntity.lower()), None)
+    #             else:
+    #                 return cutomEntity
+            
+    #         df = getFeaturesDetails[0]
+    #         df['NEW_CUSTOM_ENTITY'] = df.apply(getNameFromFeatuesList, axis=1)
+    #         title = 'Most Used ' + productNameSelect + ' Features'
+    #         fig = px.bar(df, x = "NEW_CUSTOM_ENTITY", y = "TOTAL", labels={"TOTAL" : "Total Usage", 'NEW_CUSTOM_ENTITY' : 'Features'},
+    #                     title = title, hover_data=["TOTAL"],
+    #                     template="gridon",height=500)
+    #         st.plotly_chart(fig,use_container_width=True)
+
+    packageVersion = DataBase.getPackageVersion(dbConnect, productNameSelect, st.session_state.filtes[0])
+    title = 'Package Version Usage of ' + productNameSelect
+    fig = px.bar(packageVersion[0], x = "VERSION_NAME", y = "TOTAL", labels={"TOTAL" : "Total Usage", "VERSION_NAME" : 'Package Version Name'},
+                title = title, hover_data=["TOTAL"],
+                template="gridon",height=500)
+    st.plotly_chart(fig,use_container_width=True)
 
 
 
--- db.py
+++ db.py
@@ -28,21 +28,8 @@ def resultProcess(curser):
         dataList.append(df)
     return dataList
 
-
-# def getAllProductData(connection):
-#     print('Start...')
-
-#     query1 = connection.execute_string(
-#         "select count(*) AS Over_All_Usage, log_created_month from package_usage_summary group by log_created_month;"
-#         )
-#     outData = resultProcess(query1)
-#     # print('outData:::::', outData)
-
-#     return outData
-
 def getProductDetails(connection, productName, filters):
-    print('filters :::::::::', filters)
-    queryString = "select count(*) AS OVER_ALL_USAGE, LOG_CREATED_MONTH from package_usage_summary where package_name='"+productName+"' and ORGANIZATION_EDITION != 'Developer Edition'"
+    queryString = "select sum(num_events) AS OVER_ALL_USAGE, LOG_CREATED_MONTH from package_usage_summary where package_name='"+productName+"' and ORGANIZATION_EDITION != 'Developer Edition' and organization_status='Active' AND USER_TYPE in ('CspLitePortal', 'Standard' )"
     if(filters and filters['isFilter']):
         if(filters['isOrgFilter']):
             queryString = queryString + "AND concat(REPLACE(organization_name, '''', ''), '-', organization_id) = '" + filters['orgName'] + "'"
@@ -54,47 +41,41 @@ def getProductDetails(connection, productName, filters):
 
         if(filters['isFeatureFilter']):
             queryString = queryString + "AND custom_entity = '" + filters['featuesName'] + "' "
+        elif(filters['isFeatureFilter'] == False):
+            queryString = queryString + "AND custom_entity = 'ListViewController' "
+
 
     queryString = queryString + " group by LOG_CREATED_MONTH order by LOG_CREATED_MONTH ASC;"
-    # if(isOrgFilter):
-    #     queryString = "select count(*) AS OVER_ALL_USAGE, LOG_CREATED_MONTH from package_usage_summary where package_name='"+productName+"' and ORGANIZATION_EDITION != 'Developer Edition' AND concat(organization_name, '-', organization_id) =  '" + orgName+ "' group by LOG_CREATED_MONTH order by LOG_CREATED_MONTH ASC;"
-    # else:
-    #     queryString = "select count(*) AS OVER_ALL_USAGE, LOG_CREATED_MONTH from package_usage_summary where package_name='"+productName+"' and ORGANIZATION_EDITION != 'Developer Edition' group by LOG_CREATED_MONTH order by LOG_CREATED_MONTH ASC;"
-    print('queryString ::::::::::', queryString)
+    print('getProductDetails :::::::: queryString ::::::::::', queryString)
     query1 = connection.execute_string(queryString)
     
-    # "select custom_entity from  package_usage_summary where package_name='"+ productName+"' AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity;"
     outData = resultProcess(query1)
-
     return outData
 
-# def getFeatureDetails(connection, productName,isSingleValue, filteredLits):
-#     if(isSingleValue == True):
-#         query1 = connection.execute_string(
-#             "select count(*) AS OVER_ALL_USAGE, log_created_month from package_usage_summary where package_name='"+ productName+"' AND custom_entity = '"+ filteredLits +"' AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity, log_created_month order by LOG_CREATED_MONTH ASC;"
-#             )
-#     else: 
-#         query1 = connection.execute_string(
-#             "select count(*) AS OVER_ALL_USAGE, log_created_month from package_usage_summary where package_name='"+ productName+"' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity, log_created_month order by LOG_CREATED_MONTH ASC;".format(filteredLits)
-#             )
-#     outData = resultProcess(query1)
+def getnumberOfUsers(connection, productName, filters):
+    print('filters ::::::::::', filters)
 
-#     print('outData ::::::', outData)
+    queryString = "select count(USER_COUNT) OVER_ALL_USAGE, log_created_month from( select COUNT(DISTINCT USER_ID_TOKEN) AS USER_COUNT,log_created_month from package_usage_summary where package_name='"+productName+"' and ORGANIZATION_EDITION != 'Developer Edition' and organization_status='Active' AND USER_TYPE in ('CspLitePortal', 'Standard' ) AND custom_entity = 'ListViewController'"
 
-#     return outData
+    
+    print('queryString :::::::::', queryString)
+    if(filters and filters['isFilter']):
+        if(filters['isOrgFilter']):
+            queryString = queryString + "AND concat(REPLACE(organization_name, '''', ''), '-', organization_id) = '" + filters['orgName'] + "'"
 
-def getnumberOfCreates(connection, productName, isSingleValue, filteredLits):
-    if(isSingleValue == True):
-        query1 = connection.execute_string(
-            "select sum(num_creates) AS SUM_OF_CREATES from package_usage_summary where package_name='"+ productName+"' AND custom_entity = '"+ filteredLits +"' AND ORGANIZATION_EDITION != 'Developer Edition';"
-            )
-    else: 
-        query1 = connection.execute_string(
-            "select sum(num_creates) AS SUM_OF_CREATES from package_usage_summary where package_name='"+ productName+"' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition';".format(filteredLits)
-            )
+        if(filters['isDateFromFilter'] and filters['isDateToFilter']):
+            fromDate = filters['fromDate']
+            toDate = filters['toDate']
+            queryString = queryString + "AND LOG_CREATED_MONTH >= '" + fromDate[:7] +"' AND LOG_CREATED_MONTH <= '" + toDate[:7] +"' "
+
+    queryString = "group by user_id_token, log_created_month ) group by log_created_month order by log_created_month limit 2;"
+
+    print('getnumberOfCreates :::::::: queryString ::::::::::', queryString)
+
+    query1 = connection.execute_string(queryString)
+    
     outData = resultProcess(query1)
-
-    print('outData ::::::', outData)
+    print('outData ::::::::::::::::::::::', outData)
 
     return outData
 
@@ -131,27 +112,13 @@ def getnumberOfDeletes(connection, productName, isSingleValue, filteredLits):
 def getFeatureDetails(connection, productName, filteredLits, isOrgFilter, orgName):
 
     if(isOrgFilter):
-        queryString = "select count(*) AS total,custom_entity from package_usage_summary where package_name='"+ productName+"' AND concat(organization_name, '-', organization_id) =  '" + orgName+ "' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity order by total DESC;".format(filteredLits)
+        queryString = "select sum(num_events) AS total,custom_entity from package_usage_summary where package_name='"+ productName+"' AND concat(organization_name, '-', organization_id) =  '" + orgName+ "' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity order by total DESC;".format(filteredLits)
     else:
-        queryString = "select count(*) AS total,custom_entity from package_usage_summary where package_name='"+ productName+"' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity order by total DESC;".format(filteredLits)
+        queryString = "select sum(num_events) AS total,custom_entity from package_usage_summary where package_name='"+ productName+"' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition' group by custom_entity order by total DESC;".format(filteredLits)
     query1 = connection.execute_string(queryString)
     outData = resultProcess(query1)
 
-    # print('outData ::::::::::::::',outData)
-
     return outData
-
-
-# def getTopAndLeast(connection, productName):
-#     print('productName ::::::::::::::', productName)
-
-#     query1 = connection.execute_string(
-#         "select count(*) AS total,custom_entity from package_usage_summary where package_name='"+ productName+"'  group by custom_entity order by total DESC limit 15;"
-#         "select count(*) AS total,custom_entity from package_usage_summary where package_name='"+ productName+"'  group by custom_entity order by total ASC limit 15;"
-#         )
-#     outData = resultProcess(query1)
-
-#     return outData
 
 def getPackageVersion(connection, productName, filters):
     queryString = "select count(*) AS TOTAl,VERSION_NAME  from (select VERSION_NAME,organization_name from subscriber_snapshot where package_name='"+ productName+"' AND organization_status= 'ACTIVE'"
@@ -180,27 +147,50 @@ def getPackageVersion(connection, productName, filters):
 
 def getOrganizationName(connection, productName):
 
-    query1 = connection.execute_string(
-        "select REPLACE(organization_name, '''', '') as organization_name ,organization_id from package_usage_summary where package_name='"+ productName+"' AND ORGANIZATION_EDITION != 'Developer Edition'  group by organization_name, organization_id order by organization_name ASC"
-        )
-            # "select count(*) AS TOTAL,VERSION_NAME  from (select VERSION_NAME,organization_name from subscriber_snapshot where package_name='"+ productName+"' group by VERSION_NAME, organization_name) group by VERSION_NAME;"
-
+    queryString = "select REPLACE(organization_name, '''', '') as organization_name ,organization_id from package_usage_summary where package_name='"+ productName+"' AND ORGANIZATION_EDITION != 'Developer Edition'  and organization_status='Active' group by organization_name, organization_id order by organization_name ASC"
+    query1 = connection.execute_string(queryString)
     outData = resultProcess(query1)
-
-    # print('outData :::::::::::', outData)
-
     return outData
 
+def getProductDetailsWithfeature(connection, productName, filters, filteredLits):
+
+    queryString = "select sum(num_events) AS OVER_ALL_USAGE, LOG_CREATED_MONTH, custom_entity from package_usage_summary where package_name='"+ productName+"' AND custom_entity IN {} AND ORGANIZATION_EDITION != 'Developer Edition' ".format(filteredLits)
+    if(filters and filters['isFilter']):
+        if(filters['isOrgFilter']):
+            queryString = queryString + "AND concat(REPLACE(organization_name, '''', ''), '-', organization_id) = '" + filters['orgName'] + "'"
+
+        if(filters['isDateFromFilter'] and filters['isDateToFilter']):
+            fromDate = filters['fromDate']
+            toDate = filters['toDate']
+            queryString = queryString + "AND LOG_CREATED_MONTH >= '" + fromDate[:7] +"' AND LOG_CREATED_MONTH <= '" + toDate[:7] +"' "
 
 
+    queryString = queryString + "group by LOG_CREATED_MONTH, custom_entity order by OVER_ALL_USAGE DESC;"
+    
+    print('getProductDetailsWithfeature ::::::::::::::::: queryString ::::::::::', queryString)
+    query1 = connection.execute_string(queryString)
+    
+    outData = resultProcess(query1)
+    return outData
 
+def getFeaturesUsageWithOrg(connection, productName, filters):
 
-# "select custom_entity from  package_usage_summary where package_name='"+ productName+"' group by custom_entity;"
-# "select count(*) AS OVER_ALL_USAGE, LOG_CREATED_MONTH from package_usage_summary where package_name='"+productName+"' group by LOG_CREATED_MONTH order by LOG_CREATED_MONTH ASC;"
-# "select count(*) AS total from (select *  from appanalytics.syncout.license where package_name='"+productName+"' AND internal_status IN ('Internal', 'Customer', 'Process', 'Lost', 'On-hold', 'Partner') AND ORG_TYPE  != 'Developer Edition');"
-# "select count(*) AS production from (select *  from appanalytics.syncout.license where package_name='"+productName+"' AND internal_status IN ('Internal', 'Customer', 'Process', 'Lost', 'On-hold', 'Partner')  AND subscriber_org_is_sandbox = false AND ORG_TYPE  != 'Developer Edition');"
-# "select count(*) AS sandbox from (select *  from appanalytics.syncout.license where package_name='"+productName+"' AND internal_status IN ('Internal', 'Customer', 'Process', 'Lost', 'On-hold', 'Partner')  AND subscriber_org_is_sandbox = true AND ORG_TYPE  != 'Developer Edition');"
-# "select count(*) AS active_account from (select *  from appanalytics.syncout.license where package_name='"+productName+"'  AND status= 'Active' AND internal_status IN ('Internal', 'Customer', 'Process', 'Lost', 'On-hold', 'Partner') AND subscriber_org_is_sandbox = false AND ORG_TYPE  != 'Developer Edition');"
-# "select count(*) AS trial_account from (select *  from appanalytics.syncout.license where package_name='"+productName+"'  AND status= 'Trial' AND internal_status IN ('Internal', 'Customer', 'Process', 'Lost', 'On-hold', 'Partner') AND subscriber_org_is_sandbox = false AND ORG_TYPE  != 'Developer Edition');"
-# "select count(*) AS uninstall from (select *  from appanalytics.syncout.license where package_name='"+productName+"'   AND status= 'Uninstalled' AND internal_status IN ('Internal', 'Customer', 'Process', 'Lost', 'On-hold', 'Partner') AND ORG_TYPE  != 'Developer Edition')"
+    queryString = "select sum(num_events) AS OVER_ALL_USAGE, organization_name, LOG_CREATED_MONTH from package_usage_summary  where package_name='"+ productName+"' "
+    if(filters and filters['isFilter']):
 
+        if(filters['isDateFromFilter'] and filters['isDateToFilter']):
+            fromDate = filters['fromDate']
+            toDate = filters['toDate']
+            queryString = queryString + "AND LOG_CREATED_MONTH >= '" + fromDate[:7] +"' AND LOG_CREATED_MONTH <= '" + toDate[:7] +"' "
+        
+        if(filters['isFeatureFilter']):
+            queryString = queryString + "AND custom_entity = '" + filters['featuesName'] + "' "
+        
+
+    queryString = queryString + "group by LOG_CREATED_MONTH, organization_name order by OVER_ALL_USAGE DESC;"
+    
+    print('getProductDetailsWithfeature ::::::::::::::::: queryString ::::::::::', queryString)
+    query1 = connection.execute_string(queryString)
+    
+    outData = resultProcess(query1)
+    return outData
